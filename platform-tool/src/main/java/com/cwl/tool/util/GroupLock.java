aa6df2b7ee47e7d1bb9202ada24dfebefa96bd78
dev-v <250912570@qq.com>
2019-10-12 09:03:50 +0800

    组锁

---
+++ platform-tool/src/main/java/com/cwl/tool/util/GroupLock.java
@@ -0,0 +1,40 @@
+package com.cwl.tool.util;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class GroupLock implements Lock {
+
+    private ReentrantLock[] locks;
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long l, TimeUnit timeUnit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
