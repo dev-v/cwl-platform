c1f9dec8bdeda2967659d78df3991c670a7d435b
dev-v <250912570@qq.com>
2019-10-14 20:37:32 +0800

    调整zk，以便更合理的使用

--- platform-tool/src/main/java/com/cwl/tool/zk/ZKNodeFactory.java
+++ platform-tool/src/main/java/com/cwl/tool/zk/ZKNodeFactory.java
@@ -1,5 +1,13 @@
 package com.cwl.tool.zk;
 
+import com.alibaba.fastjson.JSONObject;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryForever;
+
 /**
  * <pre>
  *
@@ -8,5 +16,74 @@ package com.cwl.tool.zk;
  * @author chenwl 2019/10/14
  */
 public class ZKNodeFactory {
+
+  private static final Log log = LogFactory.getLog(ZKNodeFactory.class);
+
+  final CuratorFramework client;
+
+  public ZKNodeFactory(ZKConfig config) {
+    client = CuratorFrameworkFactory.newClient(config.getHost(),
+        new RetryForever(config.getRetryConnectionIntervalSeconds() * 1000));
+    client.start();
+    try {
+      client.blockUntilConnected();
+    } catch (InterruptedException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+
+  public final ZKNode getRootNodeInstance(String rootPath) {
+    return saveNodeInstance(rootPath, null);
+  }
+
+  <D> ZKNodeImpl<D> saveNodeInstance(String path, D data) {
+    try {
+      if (data == null) {
+        client.createContainers(path);
+      } else {
+        Object res = client.checkExists().forPath(path);
+        byte[] bytes = JSONObject.toJSONString(data).getBytes();
+        if (res == null) {
+          log.info("zookeeper-创建节点:" + path);
+          client.create().creatingParentsIfNeeded().forPath(path, bytes);
+        } else {
+          log.info("zookeeper-重置节点:" + path);
+          client.setData().forPath(path, bytes);
+        }
+      }
+      ZKNodeImpl nodeInstance = new ZKNodeImpl();
+      nodeInstance.factory = this;
+      nodeInstance.path = path;
+      nodeInstance.v = data;
+      return nodeInstance;
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  void delete(String path) {
+    try {
+      client.delete().deletingChildrenIfNeeded().forPath(path);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  boolean exist(String path) {
+    try {
+      return client.checkExists().forPath(path) != null;
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  List<String> children(String path) {
+    try {
+      return client.getChildren().forPath(path);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
 
 }
--- platform-tool/src/main/java/com/cwl/tool/zk/ZKNodeImpl.java
+++ platform-tool/src/main/java/com/cwl/tool/zk/ZKNodeImpl.java
@@ -3,8 +3,6 @@ package com.cwl.tool.zk;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryForever;
 import org.apache.zookeeper.CreateMode;
 
 import java.util.HashMap;
@@ -18,36 +16,19 @@ import java.util.Map;
  * @author chenwl 2019/10/14
  */
 public class ZKNodeImpl<V> implements ZKNode<V> {
+
   private static final Log log = LogFactory.getLog(ZKNodeImpl.class);
   ZKNode parent;
-  CuratorFramework client;
+  ZKNodeFactory factory;
   V v;
   String path;
 
   private static final Map<String, CuratorFramework> curatorFrameworks = new HashMap<>();
 
 
-  public static final ZKNode getRootNodeInstance(ZKConfig config) {
-    CuratorFramework client = curatorFrameworks.get(config.getHost());
-    if (client == null) {
-      client = CuratorFrameworkFactory.newClient(config.getHost(),
-              new RetryForever(config.getRetryConnectionIntervalSeconds() * 1000));
-      client.start();
-      try {
-        client.blockUntilConnected();
-      } catch (InterruptedException e) {
-        log.error(e);
-        throw new IllegalStateException(e);
-      }
-      curatorFrameworks.put(config.getHost(), client);
-    }
-    return ZKUtil.saveNodeInstance(client, config.getRootPath(), null);
-  }
-
-
   @Override
   public <CV> ZKNode<CV> save(String childPath, CV val) {
-    ZKNodeImpl instance = ZKUtil.saveNodeInstance(client, path + childPath, val);
+    ZKNodeImpl instance = factory.saveNodeInstance(path + childPath, val);
     instance.parent = this;
     return instance;
   }
@@ -59,23 +40,23 @@ public class ZKNodeImpl<V> implements ZKNode<V> {
 
   @Override
   public void save(V val) {
-    ZKUtil.saveNodeInstance(client, path, val);
+    factory.saveNodeInstance(path, val);
     this.v = val;
   }
 
   @Override
   public void delete(String childPath) {
-    ZKUtil.delete(client, path + childPath);
+    factory.delete(path + childPath);
   }
 
   @Override
   public void delete() {
-    ZKUtil.delete(client, path);
+    factory.delete(path);
   }
 
   @Override
   public boolean exist(String childPath) {
-    return ZKUtil.exist(client, path + childPath);
+    return factory.exist(path + childPath);
   }
 
   @Override
@@ -96,7 +77,7 @@ public class ZKNodeImpl<V> implements ZKNode<V> {
   @Override
   public boolean createEphemeralNode(String path) {
     try {
-      client.create().withMode(CreateMode.EPHEMERAL).forPath(this.path + path);
+      factory.client.create().withMode(CreateMode.EPHEMERAL).forPath(this.path + path);
       return true;
     } catch (Exception e) {
       return false;
